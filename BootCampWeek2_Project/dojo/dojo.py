778867e212fd861c106ea0b74dfb80f442d69163
Muhwezi Allan <muhallan1@gmail.com>
2017-04-25 19:34:45 +0300

    removed an argument from the __init__ method because it was not necessary and also inherited from cmd.Cmd to use it in docopt

--- BootCampWeek2_Project/dojo/dojo.py
+++ BootCampWeek2_Project/dojo/dojo.py
@@ -12,13 +12,15 @@ from docopt import docopt, DocoptExit
 
 
 class Dojo(cmd.Cmd):
-    def __init__(self, all_rooms):
-        self.all_rooms = all_rooms
+
+    def __init__(self):
+        self.all_rooms = []
+        self.all_people = []
 
     def create_room(self, room_type, *room_name):
         for room in room_name:
-            #create()
-            pass
+            """create()"""
+        pass
 
     def add_person(self, person_name):
         pass
