627157e0c0f9faa8468912f2ed6142d9046cbae4
Muhwezi Allan <muhallan1@gmail.com>
2017-04-25 22:16:48 +0300

    wrote the init method to initialize a Room object. Also added the print_name method to print the name of the room and a list of the occupants

---
+++ BootCampWeek2_Project/room/room.py
@@ -0,0 +1,12 @@
+"""
+A Room class that is used to create a room in the Dojo. This is the super class that is inherited by
+"""
+
+
+class Room (object):
+    def __init__(self, room_name, room_type):
+        self.room_name = room_name
+        self.room_type = room_type
+
+    def print_name (room_name, occupants):
+        pass
