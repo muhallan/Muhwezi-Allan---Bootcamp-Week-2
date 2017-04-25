31319d5fbc9165cb275cdce96caa187d0a9ddc11
Muhwezi Allan <muhallan1@gmail.com>
2017-04-25 22:21:36 +0300

    put the  method doc_strings inside the method definitions  and removed them from above the method name

--- BootCampWeek2_Project/dojo/dojo.py
+++ BootCampWeek2_Project/dojo/dojo.py
@@ -19,14 +19,15 @@ class Dojo(cmd.Cmd):
         self.all_rooms = []
         self.all_people = []
 
-    """Creates rooms in the Dojo. creates a room after receiving as arguments the type of the room and its name.
-    Multiple names can be passed and that would create as many rooms with those names"""
     def create_room(self, room_type, *room_name):
+        """Creates rooms in the Dojo. creates a room after receiving as arguments the type of the room and its name.
+            Multiple names can be passed and that would create as many rooms with those names"""
+
         room = Room()
         for room in room_name:
             """create()"""
         pass
 
-    """Adds a person to the system and allocates the person a random room"""
     def add_person(self, person_name):
+        """Adds a person to the system and allocates the person a random room"""
         pass
