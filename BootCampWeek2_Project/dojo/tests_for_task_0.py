d3b7b32c59bb68d42e05155cba86745611273c48
Muhwezi Allan <muhallan1@gmail.com>
2017-04-27 02:08:22 +0300

    added more tests for the test_create_room_successfully method

--- BootCampWeek2_Project/dojo/tests_for_task_0.py
+++ BootCampWeek2_Project/dojo/tests_for_task_0.py
@@ -1,19 +1,28 @@
-"""
-Test cases for create_room and add_person functionalities
-"""
+
 import unittest
 
-from dojo.dojo import Dojo
+from dojo import Dojo
 
 
 class TestsForTask0(unittest.TestCase):
+    """
+    Test cases for create_room and add_person functionalities
+    """
     def test_create_room_successfully(self):
+        """
+        Test if a room is created successfully with the create_room method in Dojo
+        """
         dojo = Dojo()
         initial_room_count = len(dojo.all_rooms)
-        blue_office = dojo.create_room("Blue", "office")
+
+        blue_office = dojo.create_room("office", "Blue")
         self.assertTrue(blue_office)
+
         new_room_count = len(dojo.all_rooms)
         self.assertEqual(new_room_count - initial_room_count, 1)
+
+        no_office_created = dojo.create_room("blue", "office")
+        self.assertFalse(no_office_created)
 
 if __name__ == "__main__":
     unittest.main()
