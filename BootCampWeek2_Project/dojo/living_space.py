0abea7f77d76d297559e588b78f572a64d934f09
Muhwezi Allan <muhallan1@gmail.com>
2017-04-26 14:33:23 +0300

    changed how i access the super class init method of Room in order to avoid duplication on room type since it's known by default

--- BootCampWeek2_Project/dojo/living_space.py
+++ BootCampWeek2_Project/dojo/living_space.py
@@ -3,13 +3,12 @@ from room import Room
 
 class LivingSpace(Room):
     """
-    LivingSpace class that inherits from the Room class and is used to create rooms for living in for fellows
+    LivingSpace class that inherits from the Room class and is used to create rooms
+    for living in for fellows
     """
 
-    def __init__(self, room_type, room_name):
-        super(LivingSpace, self).__init__(room_type, room_name)
-        self.room_type = "Living Space"
-        self.room_name = room_name
+    def __init__(self, room_name):
+        super(LivingSpace, self).__init__("Living Space", room_name)
         self.max_number_of_occupants = 4
 
     
