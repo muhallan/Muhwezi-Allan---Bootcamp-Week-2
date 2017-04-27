4704eb9e660c55b3c0104b8c9f45bc9e84ddb37a
Muhwezi Allan <muhallan1@gmail.com>
2017-04-27 21:22:17 +0300

    added more tests and rearranged the existing ones into separate methods

--- BootCampWeek2_Project/dojo/tests_for_task_0.py
+++ BootCampWeek2_Project/dojo/tests_for_task_0.py
@@ -4,40 +4,73 @@ import unittest
 from dojo import Dojo
 
 
-class TestsForTask0(unittest.TestCase):
+class TestsForTask0CreateRoom(unittest.TestCase):
     """
-    Test cases for create_room and add_person functionalities
+    Test cases for create_room functionalities
     """
+
+    def setUp(self):
+        """Initialize the test case class"""
+        self.dojo = Dojo()
+
     def test_create_room_successfully(self):
         """
         Test if a room is created successfully with the create_room method in Dojo
         """
-        dojo = Dojo()
-        initial_room_count = len(dojo.all_rooms)
-
-        blue_office = dojo.create_room("office", "Blue")
+        blue_office = self.dojo.create_room("office", "Blue")
         self.assertTrue(blue_office)
 
-        new_room_count = len(dojo.all_rooms)
+    def test_create_room_added_to_list(self):
+        """Test if the total room count increases after creating a room
+        """
+        initial_room_count = len(self.dojo.all_rooms)
+        blue_office = self.dojo.create_room("office", "Blue")
+        new_room_count = len(self.dojo.all_rooms)
         self.assertEqual(new_room_count - initial_room_count, 1)
 
-        no_office_created = dojo.create_room("blue", "office")
+    def test_bad_room_type_not_created(self):
+        """Test that a room is not created when the room type is not office or livingspace
+        """
+        no_office_created = self.dojo.create_room("blue", "office")
         self.assertFalse(no_office_created)
 
-        duplicate_room = dojo.create_room("office", "Blue")
+    def test_duplicate_room_not_created(self):
+        """Test that a room that has a name as an already created room is not created
+        """
+        love_space = self.dojo.create_room("livingspace", "Love")
+        duplicate_room = self.dojo.create_room("livingspace", "Love")
         self.assertEqual(duplicate_room, False)
+
+class TestsForTask0AddPerson(unittest.TestCase):
+    """Test cases for add_person functionalities
+    """
+
+    def setUp(self):
+        """Initialize the test case class"""
+        self.dojo = Dojo()
 
     def test_add_person_successfully(self):
         """
         Test if a person is added successfully using the add_person method in Dojo
         """
-        dojo = Dojo()
-        initial_people_count = len(dojo.all_people)
-
-        #add_person(self, person_name, person_type, wants_accommodation="N")
-
-        staff_one = dojo.add_person("Stan Lee", "staff")
+        staff_one = self.dojo.add_person("Stan", "Lee", "staff")
         self.assertTrue(staff_one)
+
+    def test_create_room_added_to_list(self):
+        """Test if the total people count increases after creating a people
+        """
+        initial_people_count = len(self.dojo.all_people)
+        staff_one = self.dojo.add_person("Stan", "Lee", "staff")
+        new_people_count = len(self.dojo.all_people)
+        self.assertEqual(new_people_count - initial_people_count, 1)
+
+    def test_duplicate_person_not_created(self):
+        """Test that a person that has a name as an already created person is not created
+        """
+        staff_one = self.dojo.add_person("Stan", "Lee", "staff")
+        staff_duplicate = self.dojo.add_person("Stan", "Lee", "staff")
+        self.assertEqual(staff_duplicate, False)
+
 
 
 
